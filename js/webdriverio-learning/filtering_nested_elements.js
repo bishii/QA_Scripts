77fffeb5165cefdbf6f310ab50a0e2a4b67b9b43
Brent Ishii <brentishii@Brents-MacBook-Pro.local>
2018-09-05 23:16:48 -0700

    Added asserts

--- js/webdriverio-learning/filtering_nested_elements.js
+++ js/webdriverio-learning/filtering_nested_elements.js
@@ -1,4 +1,5 @@
 var webdriverio = require('webdriverio');
+var assert = require('assert')
 var options = {
     desiredCapabilities: {
         browserName: 'firefox'
@@ -10,9 +11,13 @@ webdriverio
     .init()
     .url('http://eb-new-two.us-west-2.elasticbeanstalk.com/test/test_1')
     .element("HUMANS").getText("NAME" ).then(function(theObj) {
+	assert.equal(theObj.length, 3)
+	console.log("Verified:")
 	console.log(theObj)
 	})
     .element("ALIENS").getText("NAME[nickname='green blooded computer'").then(function(theObj) {
+	assert.equal(theObj, "Mr. Spock")
+	console.log("Verified:")
 	console.log(theObj)
 	})
     .end()
