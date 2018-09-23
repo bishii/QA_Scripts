8532c28ecd41b04826b78b09729ad38be9726f5a
pi <brent.j.ishii@gmail.com>
2018-09-22 23:03:19 -0700

    Got sorting to work!

--- IoT/RaspberryPi/LED-Matrix/AutumnCornicopia.py
+++ IoT/RaspberryPi/LED-Matrix/AutumnCornicopia.py
@@ -14,6 +14,7 @@ device = max7219(serial, height=8, width=32, block_orientation=-90, rotate=2)
 device.contrast(0)
 
 dotsSource = []
+dotsSourceOrig = []
 
 #dotsSource = [("one",2,2,(0,0)),("two",1,5,(5,0))]
 #for dotS in dotsSource:
@@ -21,25 +22,62 @@ dotsSource = []
 
 numDots = int(input("How many Dots ?"))
 
-row = 0
+row = -1
+
 for co in range(numDots):
 	count = co%32 
-	if count == 31: row +=1
-	dotsSource.append(Dot(("dot-instance-%s" % count,randrange(1,500,1)/100, randrange(1,500,1)/100, (count,row))))
+	if count == 0: row +=1
+	l1 = randrange(1,500,1)*0.01
+	l1 = 0.10
+	l2 = randrange(1,500,1)*0.01
+	theDot = Dot(("dot-instance-%s" % count,l1,l2, (count,row)))
+	dotsSource.append(theDot)
+	dotsSourceOrig.append(theDot)
 
 dotsPointers = []
 
 
 
-for duration in range(1000):
-	sleep(0.1)
+transform = False
+newX = 0
+newY = 6
+sortIntervalCounter = 0
+for duration in range(500):
+	sleep(0.2)
 	whiteDots = []
 	blackDots = []
 	with canvas(device) as draw:
-		for theDot in dotsSource:
+		for theDot in dotsSourceOrig:
 			if theDot.get_current_state() == True:
 				whiteDots.append(theDot.get_coords())
 			else:
 				blackDots.append(theDot.get_coords())	
 		draw.point(blackDots,fill="black") 
 		draw.point(whiteDots,fill="white") 
+
+	LinearSortList = []
+
+	if duration == 5: 
+		transform = True
+
+	if transform == True and len(dotsSource) > 0:
+		sortIntervalCounter +=1
+		if sortIntervalCounter > 5:
+			sortIntervalCounter = 0
+	
+			currentSmallestTrueValue = dotsSource[0].get_interval_time_true()
+		
+			for d in dotsSource:	
+				curr = d.get_interval_time_true()
+				if curr < currentSmallestTrueValue: 
+					currentSmallestTrueValue = curr 
+
+			for d in dotsSource:
+				if d.get_interval_time_true() == currentSmallestTrueValue:
+					d.move_to((newX, newY))
+					dotsSource.remove(d)
+					currentSmallestTrueValue=1000
+					break
+
+			newX += 1
+			if newX == 32: newY +=1
